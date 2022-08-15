fd2f22b1c10fdf3f9661aaa8e1c3662e4bfb95da
Nathan Osborn <67983507+Nateo73@users.noreply.github.com>
2022-08-15 19:11:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+drop_fields = ['clu_classification_code','clu_calculated_acreage','comments','data_source_site_identifier','state_code','county_code','creation_date','last_change_date',]
+add_fields = [['case_id','LONG','case_id'],['status','TEXT','status']]
